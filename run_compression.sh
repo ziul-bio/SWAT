eebf91f7fdaf8ace79ebaeb74d1ad330cb556fe5
ziul-bio <ziul.bio@gmail.com>
2024-05-16 17:10:00 -0500

    updating run_compression to loop dirs and methods

--- run_compression.sh
+++ run_compression.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
 set -e
 
-# Define the list of methods to loop through
-methods=('mean' 'bos' 'max_pool' 'pca1' 'pca2' pca1-'2' 'rbf1' 'rbf2' 'sigmoid1' 'sigmoid2')
+# Define the list of directories to loop through
+directories=('BLAT_ECOLX_Ostermeier2014_esm2_150M' 'sumo1_esm2_150M' 'pisces_esm2_150M')
 
-# Loop through each method in the list
-for method in "${methods[@]}"
+# Define the list of methods to loop through
+#methods=('mean' 'bos' 'max_pool' 'pca1' 'pca2' 'pca1-2' 'rbf1' 'rbf2' 'sigmoid1' 'sigmoid2')
+methods=('mean' 'bos' 'maxPool' 'pca1' 'pca2' 'pca1-2' 'rbf1' 'rbf2')
+
+# Loop through each directory
+for dir in "${directories[@]}"
 do
-    # Run the Python script with the current method as an argument
-    echo 'Running compression for method: ' $method
-    time python scripts/compressing_embeddings.py  -e embeddings/BLAT_ECOLX_Ostermeier2014_esm2_150M/ -c $method -l 30
-    
+    echo "Processing embeddings: $dir"
+
+    # Loop through each method in the list
+    for method in "${methods[@]}"
+    do
+        echo 'Running compression for method: ' $method
+        # Run the Python script with the current directory and method as arguments
+        time python scripts/compressing_embeddings.py -e "embeddings/${dir}/" -c $method -l 30
+    done
 done
