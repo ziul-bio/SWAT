d09e4797e4bea9dffcb25fb66c06f9bf00218ad8
ziul-bio <ziul.bio@gmail.com>
2024-07-25 14:43:33 -0500

    adding script to download and process benchmarks datasets

---
+++ run_extraction_loop_benchmarks.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+set -e
+
+files=('beta_lactamase')
+files=('subcellular_localization' 'subcellular_localization_2' 'fluorescence' 'stability' 'solubility')
+
+
+for file in "${files[@]}"
+do
+    echo "Processing embeddings: $file"
+    time python scripts/extract.py esm2_t30_150M_UR50D "data/benchmarks/${file}/${file}_test_data.fasta" "embeddings/embeddings_benchmarks/${file}_test_esm2_150M" --repr_layers 30 --include bos mean per_tok
+    time python scripts/extract.py esm2_t30_150M_UR50D "data/benchmarks/${file}/${file}_train_data.fasta" "embeddings/embeddings_benchmarks/${file}_train_esm2_150M" --repr_layers 30 --include bos mean per_tok
+    time python scripts/extract.py esm2_t30_150M_UR50D "data/benchmarks/${file}/${file}_valid_data.fasta" "embeddings/embeddings_benchmarks/${file}_valid_esm2_150M" --repr_layers 30 --include bos mean per_tok
+done
+
+
+
+       
+                
+
+
+        
+           
+
+
+
+       
+               
+
+       
+      
+        
+
+       
+             
+           
+
+         
+      
+        
+
+ 
+          
+           
+
+   
+                      
+        
+
+ 
+        
+
+   
+          
+
+           
+ 
