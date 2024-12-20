6f4e1d827de65e5eb95a42a73254c8fac0f01c76
ziul-bio <ziul.bio@gmail.com>
2024-12-20 17:04:06 -0600

    updating results notebook

--- scripts/run_lassoCV_sampling.sh
+++ scripts/run_lassoCV_sampling.sh
@@ -4,21 +4,22 @@ set -e
 # usage
 #taskset -c 50-112 bash run_lassoCV_sampling.sh 
 
-#models=('esm1v_650M' 'esm2_8M' 'esm2_35M' 'esm2_150M' 'esm2_650M' 'esm2_3B' 'esm2_15B')
+models1=('esm1v_650M' 'esm2_8M' 'esm2_35M' 'esm2_150M' 'esm2_650M' 'esm2_3B' 'esm2_15B')
 
-#models=('esmc_300M' 'esmc_600M')
-models=('amplify_120M' 'amplify_350M')
+models2=('esmc_300M' 'esmc_600M' 'amplify_120M' 'amplify_350M')
 
-for model in "${models[@]}" 
+for model in "${models2[@]}" 
 do
     echo "Running regression on embeddings with LassoCV and sampling for model $model"
     
-    # echo "BLAT ECOLX 2015"
-    # time python scripts/reg_LassoCV_sampling.py -i "embeddings/BLAT_2015_compressed/${model}/BLAT_ECOLX_2015_${model}_${method}_df.pkl" -m "data/metadata_DMS/BLAT_ECOLX_Ranganathan2015_metadata.csv" -o "results/lassoCV_BLAT2015/lassoCV_sampling/BLAT_ECOLX_2015_sampling_${model}_${method}.csv"
+    echo "BLAT ECOLX 2015"
+    time python scripts/reg_LassoCV_sampling.py -i "embeddings/DMS_compressed/${model}/BLAT_ECOLX_Ranganathan2015/embed_BLAT_ECOLX_Ranganathan2015_mean.pt" -m "data/DMS_metadata/BLAT_ECOLX_Ranganathan2015_metadata.csv" -o "results/lassoCV_sampling/BLAT_ECOLX_2015/BLAT_ECOLX_2015_sampling_${model}_mean.csv"
     
-    # echo "PABP doubles"
-    # time python scripts/reg_LassoCV_sampling.py -i "embeddings/PABP_doubles_compressed/${model}/PABP_doubles_${model}_${method}_df.pkl" -m "data/metadata_DMS/PABP_YEAST_Fields2013_doubles_metadata.csv" -o "results/lassoCV_PABP_doubles/lassoCV_sampling/PABP_doubles_sampling_${model}_${method}.csv"
+    echo "PABP doubles"
+    time python scripts/reg_LassoCV_sampling.py -i "embeddings/DMS_compressed/${model}/PABP_YEAST_Fields2013_doubles/embed_PABP_YEAST_Fields2013_doubles_mean.pt" -m "data/DMS_metadata/PABP_YEAST_Fields2013_doubles_metadata.csv" -o "results/lassoCV_sampling/PABP_doubles/PABP_doubles_sampling_${model}_mean.csv"
                                                                                                                                                                            
     # echo "HIS7"
-    time python scripts/reg_LassoCV_sampling.py -i "embeddings/HIS7_compressed/${model}/embed_HIS7_mean.pt" -m "data/HIS7_metadata/HIS7_YEAST_Kondrashov2017_metadata.csv" -o "results/lassoCV_sampling/HIS7/HIS7_sampling_${model}_mean.csv"
+    #time python scripts/reg_LassoCV_sampling.py -i "embeddings/HIS7_compressed/${model}/embed_HIS7_mean.pt" -m "data/HIS7_metadata/HIS7_YEAST_Kondrashov2017_metadata.csv" -o "results/lassoCV_sampling/HIS7/HIS7_sampling_${model}_mean.csv"
 done
+
+
